b1065092d18af495427ce10e545930209bd46630
krilklem <rafi.panyotova@gmail.com>
2025-12-10 17:40:35 +0000

    feat: Receive data to save from the front

--- backend/src/controllers/supabase.controllers.ts
+++ backend/src/controllers/supabase.controllers.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import supabase from "../services/supabase.service.js";
 
 export const save = async (req: Request, res: Response):Promise<Response> => {
-    const { data, error } = await supabase.from("test_relation").select("*");
-    return res.status(200).json({ data });
+    const { resume, jobDescription, feedback, cvName } = req.body;
+    //const { data, error } = await supabase.from("test_relation").select("*");
+    return res.status(200).json({ message: "received files to save" });
 };
