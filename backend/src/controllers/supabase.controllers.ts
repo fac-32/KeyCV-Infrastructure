23fbc35c73322bf42df0ef550f367a498a78d27e
krilklem <rafi.panyotova@gmail.com>
2025-12-11 14:36:54 +0000

    fix: Remove connection to supabase as all interfacing will be done through the front

--- backend/src/controllers/supabase.controllers.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from "express";
-import supabase from "../services/supabase.service.js";
-
-export const save = async (req: Request, res: Response):Promise<Response> => {
-    const { resume, jobDescription, feedback, cvName } = req.body;
-    //const { data, error } = await supabase.from("test_relation").select("*");
-    return res.status(200).json({ message: "received files to save" });
-};
--- backend/src/routes/supabase.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { save } from "../controllers/supabase.controllers.js";
-
-const router = Router();
-
-router.post("/save", save);
-
-export default router;
