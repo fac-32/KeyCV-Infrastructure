e116c5bfd153293d1965ff01d2b90e7573cfe42f
krilklem <rafi.panyotova@gmail.com>
2025-12-16 12:18:09 +0000

    feat: Pass original file name as cvName to front end

--- backend/src/controllers/ai.controller.ts
+++ backend/src/controllers/ai.controller.ts
@@ -53,7 +53,7 @@ export const analyzeResume = async (
 
     res
       .status(200)
-      .json({ resumeText: cleanResumeText, jobDescription, feedback: result });
+      .json({ resumeText: cleanResumeText, jobDescription, feedback: result, cvName: req.file?.originalname.split(".")[0] });
   } catch (error) {
     console.error("Error in analyzeResume:", error);
     res.status(500).json({
